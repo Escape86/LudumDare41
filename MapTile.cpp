3251465b58f1d1c04faaa49502e1d07377e8d284
Escape <beau.cooper86@gmail.com>
2018-04-23 16:15:57 -0700

    - Added pisser map and hooked up teleporters.

--- MapTile.cpp
+++ MapTile.cpp
@@ -57,7 +57,7 @@ std::map<int, TileInfo> westernTileIdToInfoLookup		// { id, { spriteSheetRowOffs
 std::map<int, TileInfo> bankTileIdToInfoLookup		// { id, { spriteSheetRowOffset, spriteSheetColumnOffset, walkAble }}
 {
 	{ 0,  { 0, 0, false } },
-	{ 1,  { 0, 1, false } },
+	{ 1,  { 0, 1, true } },
 	{ 2,  { 0, 2, true } },
 	{ 3,  { 0, 3, true } },
 	{ 4,  { 0, 4, false } },
@@ -97,7 +97,7 @@ std::map<int, TileInfo> bankTileIdToInfoLookup		// { id, { spriteSheetRowOffset,
 std::map<int, TileInfo> catHouseTileIdToInfoLookup		// { id, { spriteSheetRowOffset, spriteSheetColumnOffset, walkAble }}
 {
 	{ 0,{ 0, 0, false } },
-	{ 1,{ 0, 1, false } },
+	{ 1,{ 0, 1, true } },
 	{ 2,{ 0, 2, true } },
 	{ 3,{ 0, 3, true } },
 	{ 4,{ 0, 4, false } },
@@ -137,7 +137,7 @@ std::map<int, TileInfo> catHouseTileIdToInfoLookup		// { id, { spriteSheetRowOff
 std::map<int, TileInfo> houseInterriorTileIdToInfoLookup		// { id, { spriteSheetRowOffset, spriteSheetColumnOffset, walkAble }}
 {
 	{ 0,{ 0, 0, false } },
-	{ 1,{ 0, 1, false } },
+	{ 1,{ 0, 1, true } },
 	{ 2,{ 0, 2, true } },
 	{ 3,{ 0, 3, true } },
 	{ 4,{ 0, 4, false } },
@@ -177,7 +177,7 @@ std::map<int, TileInfo> houseInterriorTileIdToInfoLookup		// { id, { spriteSheet
 std::map<int, TileInfo> saloonTileIdToInfoLookup		// { id, { spriteSheetRowOffset, spriteSheetColumnOffset, walkAble }}
 {
 	{ 0,{ 0, 0, false } },
-	{ 1,{ 0, 1, false } },
+	{ 1,{ 0, 1, true } },
 	{ 2,{ 0, 2, true } },
 	{ 3,{ 0, 3, true } },
 	{ 4,{ 0, 4, false } },
@@ -252,7 +252,46 @@ std::map<int, TileInfo> moonTileIdToInfoLookup		// { id, { spriteSheetRowOffset,
 	{ 33,{ 5, 3, false } },
 	{ 34,{ 5, 4, false } },
 	{ 35,{ 5, 5, false } },
+};
 
+std::map<int, TileInfo> pisserTileIdToInfoLookup		// { id, { spriteSheetRowOffset, spriteSheetColumnOffset, walkAble }}
+{
+	{ 0,{ 0, 0, false } },
+	{ 1,{ 0, 1, true } },
+	{ 2,{ 0, 2, true } },
+	{ 3,{ 0, 3, true } },
+	{ 4,{ 0, 4, false } },
+	{ 5,{ 0, 5, false } },
+	{ 6,{ 1, 0, false } },
+	{ 7,{ 1, 1, false } },
+	{ 8,{ 1, 2, false } },
+	{ 9,{ 1, 3, false } },
+	{ 10,{ 1, 4, false } },
+	{ 11,{ 1, 5, false } },
+	{ 12,{ 2, 0, false } },
+	{ 13,{ 2, 1, false } },
+	{ 14,{ 2, 2, false } },
+	{ 15,{ 2, 3, false } },
+	{ 16,{ 2, 4, false } },
+	{ 17,{ 2, 5, false } },
+	{ 18,{ 3, 0, false } },
+	{ 19,{ 3, 1, true } },
+	{ 20,{ 3, 2, true } },
+	{ 21,{ 3, 3, true } },
+	{ 22,{ 3, 4, false } },
+	{ 23,{ 3, 5, false } },
+	{ 24,{ 4, 0, true } },
+	{ 25,{ 4, 1, true } },
+	{ 26,{ 4, 2, true } },
+	{ 27,{ 4, 3, false } },
+	{ 28,{ 4, 4, false } },
+	{ 29,{ 4, 5, false } },
+	{ 30,{ 5, 0, false } },
+	{ 31,{ 5, 1, false } },
+	{ 32,{ 5, 2, false } },
+	{ 33,{ 5, 3, false } },
+	{ 34,{ 5, 4, false } },
+	{ 35,{ 5, 5, false } },
 };
 
 std::map<std::string, int> mapFileNameToMapUniqueIDLookup
@@ -263,6 +302,7 @@ std::map<std::string, int> mapFileNameToMapUniqueIDLookup
 	{ "resources/saloon.csv", 3 },
 	{ "resources/house_interrior.csv", 4 },
 	{ "resources/moon.csv", 5 },
+	{ "resources/pisser.csv", 6 },
 };
 
 std::map<int, std::map<int, TileInfo>> mapFileNameToTileIdToInfoLookup
@@ -273,6 +313,7 @@ std::map<int, std::map<int, TileInfo>> mapFileNameToTileIdToInfoLookup
 	{ 3, saloonTileIdToInfoLookup },
 	{ 4, houseInterriorTileIdToInfoLookup },
 	{ 5, moonTileIdToInfoLookup },
+	{ 6, pisserTileIdToInfoLookup },
 };
 
 #pragma region Constructor
